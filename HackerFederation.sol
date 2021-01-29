c2e866ebb4efbca67d9d68b9e40d7e03b7592565
qq79324055 <78137253+qq79324055@users.noreply.github.com>
2021-01-29 16:04:47 +0800

    Update HackerFederation.sol
    bugfixs

--- HackerFederation.sol
+++ HackerFederation.sol
@@ -11,153 +11,147 @@ interface Token {
 contract HackerFederation {
     using SafeMath for uint256;
 
-    // 控制 he1 与 usdt 的比值
+    // HE-1 decimals
     uint256 public usdtDecimals = 0;
-    // 算力小数点位数
+    // Hashrate decimals
     uint256 public constant hashRateDecimals = 5;
-    // 每 10 usdt = 1 T
+    // 10 usdt = 1 T
     uint256 public constant hashRatePerUsdt = 10;
-    //
+    // Manager address
     address public owner;
-    // 顶点地址
+    // Root address
     address public rootAddress = 0x3585762FBFf4b2b7D92Af16b2BCfa90FE3562087;
-    // 销毁地址
+    // Burn address
     address public burnAddress = 0xC206F4CC6ef3C7bD1c3aade977f0A28ac42F3E37;
 
-    // dai 对 he3 币对 address
+    // DAI-HE3 pair address
     address public daiToHe3Address = address(0x002b48f9e6ca23e71b6cf52199db9a7c1a97cd0cd7);
 
-    // Dai erc20 代币地址
+    // DAI ERC20 address
     address public daiTokenAddress = 0x97509bF13910E046f7000ECa4fE06354E11f670A;
     Token tokenDai = Token(daiTokenAddress);
-    // HE3 erc20 代币地址
+    // HE-3 ERC20 address
     address public he3TokenAddress = 0x7559A5416CED9059411Db88DAA5fEb34f140d7D5;
     Token tokenHe3 = Token(he3TokenAddress);
 
-    // HE1 erc20 代币地址
+    // HE-1 ERC20 address
     address public he1TokenAddress = 0xF72e4d8B029c4fC6a97c59EC3AF33b2cCcC52715;
 
-    // 用户信息
+    // Userinfo
     struct User {
         address superior;
         uint256 hashRate;
         bool isUser;
     }
-    // 保存用户信息对应关系
     mapping(address => User) public users;
 
-    // 用户算力购买情况事件
+    // Buy hashrate event
     event LogBuyHashRate(address indexed owner, address indexed superior, uint256 hashRate);
 
     constructor() public {
-        //
+        // Init owner
         owner = msg.sender;
     }
 
-    // 函数修改器，只有 owner 满足条件
+    // Modifier func
     modifier onlyOwner() {
         require(msg.sender == owner, "This function is restricted to the owner");
         _;
     }
 
     /**
-     * 用户使用 he1 购买算力
-     * 需要该用户拥有 HE-1 代币
+     * Use HE-1 to buy hashrate
      *
      * Requirements:
      *
-     * - `_tokenAmount` 使用 token 数量购买算力
-     * - `_superior` 直接上级
+     * - `_tokenAmount`: Amount of HE-1 
+     * - `_superior`: User's inviter
      */
     function buyHashRateWithHE1(uint256 _tokenAmount, address _superior) public {
-        // 由于 he1 等价于 usdt，但是目前 usdt 在主网上是 6 位小数，由于不确定 he1 的小数位，目前使用变量 usdtDecimals 来进行控制
-        // 当 he1 的小数位 = 6 时，usdtDecimals = 0
+        // 
         uint256 total = _tokenAmount.div(10 ** usdtDecimals);
         //
         _buyHashRate(he1TokenAddress, _tokenAmount, total, _superior);
     }
 
     /**
-     * 用户使用 he3 购买算力
-     * 需要该用户拥有 HE-3 代币
+     * Use HE-3 to buy hashrate
      *
      * Requirements:
      *
-     * - `_tokenAmount` 使用 token 数量购买算力
-     * - `_superior` 直接上级
+     * - `_tokenAmount`: Amount of HE-3
+     * - `_superior`: User's inviter
      */
     function buyHashRateWithHE3(uint256 _tokenAmount, address _superior) public {
-        // totalDai 含有 18 位小数，但是目前 usdt 在主网上是 6 位小数
+        // 
         uint256 totalDai = getHe3ToDai(_tokenAmount);
         //
         _buyHashRate(he3TokenAddress, _tokenAmount, totalDai.div(10**12), _superior);
     }
 
     /**
-     * 用户购买算力
-     * 需要该用户拥有 HE-1 或者 HE-3 代币
+     * Buy hashrate
      *
      * Requirements:
      *
-     * - `_token` HE-1 或者 HE-3 的合约地址
-     * - `_tokenAmount` 使用 token 数量购买算力
-     * - `_usdtAmount` _tokenAmount 与 usdt 的价格
-     * - `_superior` 直接上级
+     * - `_token`: HE-1 or HE-3 address
+     * - `_tokenAmount`: Amount of token
+     * - `_usdtAmount`: Value of _tokenAmount to USDT
+     * - `_superior`: inviter
      */
     function _buyHashRate(address _tokenAddress,uint256 _tokenAmount, uint256 _usdtAmount, address _superior) internal {
-        // 判断上级是否是 user 或 rootAddress，如果都不是，抛出错误
+        // require _superior
         if (!(users[_superior].isUser || _superior == rootAddress)) {
             require(false, "Superior should be a user or rootAddress");
         }
-        // 销毁对应的代币
+        
+        // burn the token sent by user
         bool sent = Token(_tokenAddress).transferFrom(msg.sender, burnAddress, _tokenAmount);
         require(sent, "Token transfer failed");
 
-        // 10 000000 USDT = 1 00000T, 10 为小数点
+        // USDT decimals = 6
         require(_usdtAmount >= 10000000, "Usdt should be great than or equal 10");
-        // 计算当前能买多少算力
+        
         uint256 hashRate = _usdtAmount.div(10).div(hashRatePerUsdt);
-        //
         if (users[msg.sender].isUser) {
-            // 再次购买，不改变直接上级，直接更新算力
             users[msg.sender].hashRate = users[msg.sender].hashRate.add(hashRate);
         } else {
-            // 第一次购买算力，更新用户信息
             users[msg.sender].superior = _superior;
             users[msg.sender].hashRate = hashRate;
             users[msg.sender].isUser = true;
         }
-        // 触发购买算力事件
+        
+        // Buy hashrate event
         emit LogBuyHashRate(msg.sender, _superior, hashRate);
     }
 
-    // 更新管理员地址
+    // Update owner address
     function updateOwnerAddress(address _newOwnerAddress) public onlyOwner {
         owner = _newOwnerAddress;
     }
 
-    // 更新销毁地址
+    // Update burn address
     function updateBurnAddress(address _newBurnAddress) public onlyOwner {
         burnAddress = _newBurnAddress;
     }
 
-    // 更新 he3 合约地址
+    // update HE-3 contract address
     function updateHe3TokenAddress(address _he3TokenAddress) public onlyOwner {
         he3TokenAddress = _he3TokenAddress;
         tokenHe3 = Token(he3TokenAddress);
     }
 
-    // 更新 he1 合约地址
+    // update HE-1 contract address
     function updateHe1TokenAddress(address _he1TokenAddress) public onlyOwner {
         he1TokenAddress = _he1TokenAddress;
     }
 
-    // 更新 dai 合约地址
+    // update DAI contract address
     function updateDaiToHe3AddressAddress(address _daiToHe3Address) public onlyOwner {
         daiToHe3Address = _daiToHe3Address;
     }
 
-    // 更新 he3 对 dai 币对合约地址
+    // update DAI-HE3 uniswap pair contract address
     function updateDaiTokenAddress(address _daiTokenAddress) public onlyOwner {
         daiTokenAddress = _daiTokenAddress;
         tokenDai = Token(daiTokenAddress);
@@ -169,22 +163,18 @@ contract HackerFederation {
     }
 
     /**
-     * 判断该地址是否为用户
-     *
-     * Requirements:
-     *
-     * - `_userAddress` 用户地址
+     * Is user?
      */
     function isUser(address _userAddress) public view returns (bool) {
         return users[_userAddress].isUser;
     }
 
-    // 获取 1 个 he3 兑换多少个 dai
+    // Get amount 1 HE3 to DAI
     function getDaiPerHe3() public view returns (uint256) {
         return getHe3ToDai(10**18);
     }
 
-    // 获取 _he3Amount 个 he3 兑换多少个 dai
+    // Get amount _he3Amount HE3 to DAI 
     function getHe3ToDai(uint256 _he3Amount) internal view returns (uint256) {
         return tokenDai.balanceOf(daiToHe3Address).mul(_he3Amount).div(tokenHe3.balanceOf(daiToHe3Address));
     }
